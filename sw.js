51cdcf3f2d6fd7dcd886b65d2a93ae716ef56721
Ronaldo Kuhlmann <ronaldoalveskuhlmann@gmail.com>
2026-06-07 00:37:42 +0200

    update_3

--- sw.js
+++ sw.js
@@ -1,10 +1,28 @@
-const cacheName = "easy-loadsheet-v54";
-const assets = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
+const cacheName = "easy-loadsheet-v20260607-1";
+const assets = ["./", "./index.html", "./styles.css?v=20260607-1", "./app.js?v=20260607-1", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(cacheName).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
